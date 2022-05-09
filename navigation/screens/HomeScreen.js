c9adf8fd7f8cfa7d439d14f32d53ce0a33310984
Deividas <deiviuxs97@gmail.com>
2022-05-09 11:32:21 +0300

    pradzia

---
+++ navigation/screens/HomeScreen.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, enableLatestRenderer, Marker } from 'react-native-maps';
+import { useState } from 'react/cjs/react.production.min';
+
+
+enableLatestRenderer();
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: '100%',
+        width: 400,
+        flex:1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        },
+        map: {
+        ...StyleSheet.absoluteFillObject,
+        },
+   });
+
+export default function HomeScreen({navigation}) {
+    const [selectedLocation, setSelectedLocation] = useState();
+
+    const selectLocationHandler = event => {
+        setSelectedLocation({
+            lat: event.nativeEvent.coordinate.latitude,
+            lng: event.nativeEvent.coordinate.longitude
+        });
+    };
+
+    let markerCoordinates;
+
+    if (selectedLocation) {
+        markerCoordinates = {
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <MapView
+                onPress={selectLocationHandler}
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={styles.map}
+                region={{
+                    latitude: 55.715557,
+                    longitude: 21.144043,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}>
+                {markerCoordinates && <Marker title='Picked Location' coordinate={markerCoordinates}></Marker>}
+            </MapView>
+        </View>
+    );
+}
